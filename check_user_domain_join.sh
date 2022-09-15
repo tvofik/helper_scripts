37b784ee940644dce1e4a6f826edefa059801250
Abayomi Pelewura <17411689+tvofik@users.noreply.github.com>
2022-09-14 22:13:42 -0400

    added requirements

--- check_user_domain_join.sh
+++ check_user_domain_join.sh
@@ -1,14 +1,16 @@
-
 # 1 check if server is domain joined or not and echo the status out
 # 2 check if local user exist on server
-#   - if user already exist, do nothing
-#   - if user does not exist, create user with its password. retrieve password from SSM parameter store.
-# 3 check if domain user exist on server.
+#   - if user already exist, check if it is in the wheel group.
+#   - if user does not exist, create user with its password. retrieve password from SSM parameter store. check if it is in the whell group.
+
+# 3 check if domain user is in sudoer file on server. (check if domain user is in sudoer file)
 #   - if user already exist, do nothing.
-#   - if user does not exist, create user with its password. Retrieve password from SSM parameter store.
+#   - if user does not exist, add it.
 
 #!/bin/bash
 user="exampleuser"
+# Check if realm is installed
+
 # Check if part of domain
 if [[ $(realm --name-only list) ]]; then
     echo "Server is part of domain"
@@ -22,4 +24,5 @@ else
     echo "$user does not exist"
     password=$(aws ssm get-parameters --names $user --with-decryption --query "Parameters[].Value" --output text)
     sudo useradd $user -p $password > /dev/null 2>&1
+    #! Add the user to the Wheel group
 fi
